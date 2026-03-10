e266493b112f5ec031868eacd255b686f8376167
emmagiame <giamello26@up.edu>
2026-03-09 17:40:05 -0700

    update to new url

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  // Changing this to '/' tells the browser to look at the ROOT 
+  // of emmagiamello.com for your JS and Images.
+  base: '/', 
 })
