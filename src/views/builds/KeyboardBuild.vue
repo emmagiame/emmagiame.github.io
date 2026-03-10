259422f1c91cdf4e3a326b51ccb7a6cc7390ddee
emmagiame <giamello26@up.edu>
2026-03-09 18:28:46 -0700

    updated build pages to be in limbo

--- src/views/builds/KeyboardBuild.vue
+++ src/views/builds/KeyboardBuild.vue
@@ -3,7 +3,7 @@
     <Button label="Back to Builds" icon="pi pi-arrow-left" text @click="$router.push('/projects/custombuilds')" />
     <h1 class="text-4xl font-bold mt-4">Custom Keyboard Build</h1>
     <div class="mt-8 text-lg text-gray-700">
-      <p>This build focused on a 65% layout with silent tactile switches...</p>
+      <p>To be added...</p>
       </div>
   </div>
 </template>
