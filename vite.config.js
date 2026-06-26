48c2649028b3312f5c0ff9e3903fb3d2f243fe2b
emmagiame <giamello26@up.edu>
2026-06-25 20:11:19 -0700

    resume

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ export default defineConfig({
   plugins: [vue()],
   // Changing this to '/' tells the browser to look at the ROOT 
   // of emmagiamello.com for your JS and Images.
-  base: '/', 
+  base: './', 
 })
